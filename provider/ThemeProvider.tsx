49db96dd90dba2df106a718b71d129ac561bd287
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-13 15:38:52 +0600

    Override ant-design components done

--- provider/ThemeProvider.tsx
+++ provider/ThemeProvider.tsx
@@ -2,20 +2,29 @@ import { ConfigProvider } from 'antd';
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const primaryColor: string = '#102C57'; // #35374B
+  const defaultButtonBg: string = '#D7D7D7';
+  const lihkColor: string = '#401F71';
 
   return (
     <ConfigProvider
       theme={{
         token: {
+          borderRadius: 0,
           colorPrimary: primaryColor,
         },
         components: {
           Button: {
-            borderRadius: 0,
+            controlHeight: 44,
             controlOutline: 'none',
+            defaultBg: defaultButtonBg,
+            defaultHoverBg: defaultButtonBg,
+            defaultHoverBorderColor: 'none',
+            colorLink: lihkColor,
           },
           Input: {
-            borderRadius: 200,
+            controlHeight: 44,
+            controlOutline: 'none',
+            activeShadow: 'none',
           },
         },
       }}
