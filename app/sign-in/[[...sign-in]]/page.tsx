2dd3c2d4c734f489298fd1d33fc84832876876cb
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-14 15:56:28 +0600

    sign-in page customising

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,11 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className='flex items-center justify-center py-6 px-4 h-screen overflow-hidden'>
-      <SignIn />
+    <div className='flex items-center w-full bg-slate-200 h-screen overflow-hidden'>
+      <div className='w-1/2'>test</div>
+      <div className='flex items-center justify-center py-6 px-4 w-1/2 h-screen overflow-hidden'>
+        <SignIn />
+      </div>
     </div>
   );
 }
