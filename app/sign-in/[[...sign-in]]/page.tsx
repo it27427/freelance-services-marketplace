cd206e3ec140371b35a6813721d8f54c339834f6
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-14 16:29:13 +0600

    sign-in updated

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,24 @@
+'use client';
+import signInThumb from '@/assets/images/auth/login.svg';
 import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <div className='flex items-center w-full bg-slate-200 h-screen overflow-hidden'>
-      <div className='w-1/2'>test</div>
-      <div className='flex items-center justify-center py-6 px-4 w-1/2 h-screen overflow-hidden'>
+      <div className='lg:w-1/2 hidden lg:flex h-full items-center justify-center'>
+        <figure>
+          <Image
+            src={signInThumb}
+            alt='login-thumbnail'
+            priority
+            width='300'
+            height='300'
+            className='w-full h-full object-cover'
+          />
+        </figure>
+      </div>
+      <div className='flex items-center justify-center py-6 px-4 w-full lg:w-1/2 h-full overflow-hidden'>
         <SignIn />
       </div>
     </div>
