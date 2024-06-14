0f5118ee5b573cb4cca74bdf22d233a4598afcef
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-14 16:38:47 +0600

    register updated

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,28 @@
 import { SignUp } from '@clerk/nextjs';
+import Image from 'next/image';
+
+import signUpThumb from '@/assets/images/auth/mobile-login.svg';
 
 export default function Page() {
   return (
-    <div className='flex items-center justify-center py-6 px-4 min-h-screen'>
-      <SignUp />
+    <div className='flex items-center w-full bg-slate-200 h-screen overflow-hidden'>
+      <div className='lg:w-1/2 hidden lg:flex h-full items-center justify-center'>
+        <figure>
+          <Image
+            src={signUpThumb}
+            alt='register-thumbnail'
+            priority
+            width='300'
+            height='300'
+            className='w-full h-full object-cover'
+          />
+        </figure>
+      </div>
+      <div className='flex items-center justify-center py-6 px-4 w-full lg:w-1/2 h-full lg:overflow-hidden'>
+        <div className='lg:max-h-96 lg:overflow-y-scroll'>
+          <SignUp />
+        </div>
+      </div>
     </div>
   );
 }
