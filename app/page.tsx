5b7b9eb977e7da7903eca2e94783ef610a15ed70
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-14 15:43:18 +0600

    get and view loggedin user infos

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+// import { UserButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+
 import { UserOutlined } from '@ant-design/icons';
 import { Button, Flex, Input } from 'antd';
 
-export default function Home() {
+export default async function Home() {
+  const loggedInUser = await currentUser();
+  console.log(loggedInUser);
+
+  const clerkID = loggedInUser?.id;
+  const fullName = loggedInUser?.firstName + ' ' + loggedInUser?.lastName;
+  const imageURL = loggedInUser?.imageUrl;
+  const email = loggedInUser?.emailAddresses[0].emailAddress;
+  const phone = loggedInUser?.phoneNumbers[0].phoneNumber;
+
   return (
     <div className='p-12'>
       <Flex gap='small'>
@@ -11,8 +23,23 @@ export default function Home() {
         <Button type='text'>Text Button</Button>
         <Button type='link'>Link Button</Button>
       </Flex>
-
       <Input placeholder='large size' prefix={<UserOutlined />} />
+
+      <ul>
+        <li>
+          <strong>Name: </strong> {fullName}
+        </li>
+        <li>
+          <strong>Email: </strong> {email}
+        </li>
+        <li>
+          <strong>Phone: </strong> {phone}
+        </li>
+        <li>
+          <img src={imageURL} alt='Avatar' width='100' height='100' />
+        </li>
+      </ul>
+      {/* <UserButton afterSignOutUrl='sign-in' /> */}
     </div>
   );
 }
