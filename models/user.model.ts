5bcb193fe394c77e0ed668403c064e08184d9706
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-17 00:16:26 +0600

    user updated and email added in userschema

--- models/user.model.ts
+++ models/user.model.ts
@@ -5,6 +5,12 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
   },
   { timestamps: true }
