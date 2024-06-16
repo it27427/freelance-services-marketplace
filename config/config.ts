40e98e65d5945bfcdc59e9f9e91ef6d78cbce988
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-16 10:30:38 +0600

    firebaseConfig Done

--- config/config.ts
+++ config/config.ts
@@ -1,8 +1,25 @@
-const mongodbURI = process.env.MONGODB_URI;
+const mongodbURI = process.env.NEXT_PUBLIC_MONGODB_URI;
+const firebaseapiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const firebaseAuthDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const firebaseMessagingSenderId =
+  process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const firebaseAppId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+const firebaseMeasurementId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
 
 const dev = {
   db: {
     url: mongodbURI || 'mongodb://localhost:27017/freelanceservice',
+  },
+  firebase: {
+    apiKey: firebaseapiKey,
+    authDomain: firebaseAuthDomain,
+    projectId: firebaseProjectId,
+    storageBucket: firebaseStorageBucket,
+    messagingSenderId: firebaseMessagingSenderId,
+    appId: firebaseAppId,
+    measurementId: firebaseMeasurementId,
   },
 };
 
--- config/firebase.connection.ts
+++ config/firebase.connection.ts
@@ -1,13 +1,22 @@
 import { initializeApp } from 'firebase/app';
+import config from './config';
+
+const apiKey = config.firebase.apiKey;
+const authDomain = config.firebase.authDomain;
+const projectId = config.firebase.projectId;
+const storageBucket = config.firebase.storageBucket;
+const messagingSenderId = config.firebase.messagingSenderId;
+const appId = config.firebase.appId;
+const measurementId = config.firebase.measurementId;
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCDWsnbmnDNRgLV1UyMg2kgiBDXSuCws_c',
-  authDomain: 'taskwave-eedd3.firebaseapp.com',
-  projectId: 'taskwave-eedd3',
-  storageBucket: 'taskwave-eedd3.appspot.com',
-  messagingSenderId: '1045933296012',
-  appId: '1:1045933296012:web:1d6b7739098257bbb52ea9',
-  measurementId: 'G-DNEJ4M4VGT',
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
