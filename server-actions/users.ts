4ec2773671217f91f15cd30a3e6b3a62c8814fb7
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-18 10:46:29 +0600

    import currentUser for checking user in DB

--- server-actions/users.ts
+++ server-actions/users.ts
@@ -1,11 +1,15 @@
 'use server';
-import User from '@/models/user.model';
+import { currentUser } from '@clerk/nextjs/server';
+
 import connectDB from '@/config/mongodb.connection';
+import User from '@/models/user.model';
 
 connectDB();
 
 export const getCurrentUserFromMongodb = async () => {
   try {
+    const clerkUser = await currentUser();
+    const user = await User.findOne({ clerkUserId: clerkUser?.id });
   } catch (error: any) {
     return {
       success: false,
