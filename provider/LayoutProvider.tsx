7a36103aa54aacaf1a3741380b4950ed90b0d30e
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-20 00:00:49 +0600

    tailwind config and layout responsive

--- provider/LayoutProvider.tsx
+++ provider/LayoutProvider.tsx
@@ -2,11 +2,13 @@ import Sidebar from '@/components/Sidebar';
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex gap-5 h-screen'>
-      <div className='h-full hidden lg:block lg:w-64'>
+    <div className='flex gap-5 h-screen w-full overflow-hidden'>
+      <div className='h-full lg:w-56 hidden lg:flex'>
         <Sidebar />
       </div>
-      <main className='p-5 lg:w-[calc(100%-16rem)]'>{children}</main>
+      <main className='p-5 w-full lg:w-[calc(100%-14rem)] ml-auto h-full overflow-y-scroll'>
+        {children}
+      </main>
     </div>
   );
 };
