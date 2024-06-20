47c9b2c1bf7a344bbbc41b84b91217cd03026db6
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-20 12:34:30 +0600

    MainMenu Component Created

---
+++ components/MainMenu.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { AiOutlineHome } from 'react-icons/ai';
+import { FiUser } from 'react-icons/fi';
+import { GrList } from 'react-icons/gr';
+import { MdOutlineTaskAlt } from 'react-icons/md';
+import { PiSignOut } from 'react-icons/pi';
+
+const MainMenu = () => {
+  const pathName = usePathname();
+
+  const menuItems = [
+    {
+      name: 'Home',
+      href: '/',
+      icon: <AiOutlineHome />,
+      isActive: pathName === '/',
+    },
+    {
+      name: 'Profile',
+      href: '/profile',
+      icon: <FiUser />,
+      isActive: pathName === '/profile',
+    },
+    {
+      name: 'Bids',
+      href: '/profile/bids',
+      icon: <GrList />,
+      isActive: pathName === '/profile/bids',
+    },
+    {
+      name: 'Tasks',
+      href: '/profile/tasks',
+      icon: <MdOutlineTaskAlt />,
+      isActive: pathName === '/profile/tasks',
+    },
+    {
+      name: 'Logout',
+      href: '/logout',
+      icon: <PiSignOut />,
+    },
+  ];
+
+  return (
+    <ul className='flex flex-col gap-2 list-none m-0 p-0'>
+      {menuItems.map((item, index) => (
+        <li key={index}>
+          <Link
+            href={item.href}
+            className='flex items-center gap-3 p-3 rounded no-underline text-sm'
+          >
+            {item.icon}
+            {item.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MainMenu;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,41 +1,9 @@
 import Link from 'next/link';
-import { AiOutlineHome } from 'react-icons/ai';
-import { FiUser } from 'react-icons/fi';
-import { GrList } from 'react-icons/gr';
-import { MdOutlineTaskAlt } from 'react-icons/md';
-import { PiSignOut } from 'react-icons/pi';
 
 import BrandLogo from '@/components/BrandLogo';
+import MainMenu from '@/components/MainMenu';
 
 const Sidebar = () => {
-  const menuItems = [
-    {
-      name: 'Home',
-      href: '/',
-      icon: <AiOutlineHome />,
-    },
-    {
-      name: 'Profile',
-      href: '/profile',
-      icon: <FiUser />,
-    },
-    {
-      name: 'Bids',
-      href: '/profile/bids',
-      icon: <GrList />,
-    },
-    {
-      name: 'Tasks',
-      href: '/profile/tasks',
-      icon: <MdOutlineTaskAlt />,
-    },
-    {
-      name: 'Logout',
-      href: '/logout',
-      icon: <PiSignOut />,
-    },
-  ];
-
   return (
     <aside className='p-5 w-full h-full sticky top-0 z-50 bg-gray-200'>
       <header className='flex flex-col items-center justify-center'>
@@ -46,19 +14,7 @@ const Sidebar = () => {
       </header>
 
       <div className='mt-10'>
-        <ul className='flex flex-col gap-2 list-none m-0 p-0'>
-          {menuItems.map((item, index) => (
-            <li key={index}>
-              <Link
-                href={item.href}
-                className='flex items-center gap-3 p-3 rounded no-underline text-sm'
-              >
-                {item.icon}
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <MainMenu />
       </div>
     </aside>
   );
