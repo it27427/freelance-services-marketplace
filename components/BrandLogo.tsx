3f2b2be6deb9ddc341b36d5d2e63854ce0e8fde2
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-20 11:44:37 +0600

    BrandLogo Completed

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -2,8 +2,12 @@ import Logo from '@/assets/images/logo/logo.webp';
 
 const BrandLogo = () => {
   return (
-    <figure className='flex items-center justify-center'>
-      <img src={Logo.src} alt='Brand-Logo' />
+    <figure className='flex items-center justify-center w-24 h-24 rounded-full overflow-hidden m-0'>
+      <img
+        src={Logo.src}
+        alt='Brand-Logo'
+        className='w-full h-full object-cover'
+      />
     </figure>
   );
 };
