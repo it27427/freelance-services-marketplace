e5a89a43154fcde3afec786f679a904f4d5ca9c4
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-20 11:47:44 +0600

    BrandLogo Linked

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+
 import BrandLogo from '@/components/BrandLogo';
 
 const Sidebar = () => {
   return (
     <aside className='p-5 w-full h-full sticky top-0 z-50 bg-gray-200'>
       <header className='flex flex-col gap-3 items-center justify-center'>
-        <BrandLogo />
+        <Link href='/'>
+          <BrandLogo />
+        </Link>
       </header>
     </aside>
   );
