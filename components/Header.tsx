bef502950189efc982f4a523d8833e314e41418c
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-22 13:00:20 +0600

    bg-tertiary added in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className='sticky top-0 w-full h-20 flex items-cente' style={{background: '#26355D'}}>
+    <header className='sticky top-0 w-full h-20 flex items-cente bg-tertiary'>
       <nav className='flex items-center justify-between'>
         <Link href='/'>
           <MobileLogo />
