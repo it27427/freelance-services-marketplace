2bb573a63548cb2a758af70021be2973ac02935a
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-22 11:46:27 +0600

    LayoutProvider Updated

--- provider/LayoutProvider.tsx
+++ provider/LayoutProvider.tsx
@@ -2,7 +2,7 @@ import Sidebar from '@/components/Sidebar';
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex gap-5 h-screen w-full overflow-hidden'>
+    <div className='flex flex-col md:flex-row gap-5 h-screen w-full overflow-hidden'>
       <div className='h-full lg:w-56 hidden lg:flex'>
         <Sidebar />
       </div>
