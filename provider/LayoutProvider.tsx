7b8d3be80751c90a1152e3dd76fcb33dab386c60
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-22 13:03:24 +0600

    layout updated for mobile and desktop

--- provider/LayoutProvider.tsx
+++ provider/LayoutProvider.tsx
@@ -3,11 +3,11 @@ import Sidebar from '@/components/Sidebar';
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex flex-col md:flex-row gap-5 h-screen w-full overflow-hidden'>
+    <div className='flex flex-col lg:flex-row gap-5 h-screen w-full overflow-hidden'>
       <div className='h-full lg:w-56 hidden lg:flex'>
         <Sidebar />
       </div>
-      <div className='flex md:hidden'>
+      <div className='flex lg:hidden'>
         <Header />
       </div>
       <main className='p-5 w-full lg:w-[calc(100%-14rem)] ml-auto h-full overflow-y-scroll'>
