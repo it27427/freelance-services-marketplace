9fd86fc0d8c7e73c72dea69006af1d741abc2e46
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-22 13:49:08 +0600

    header nav width added

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import MobileLogo from '@/components/MobileLogo';
 const Header = () => {
   return (
     <header className='sticky top-0 w-full h-20 flex items-cente bg-tertiary'>
-      <nav className='flex items-center justify-between'>
+      <nav className='flex items-center justify-between w-full'>
         <Link href='/'>
           <MobileLogo />
         </Link>
