6f9c6fa5d9cd6f3d6f14202817b7a16a9bb4ad56
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-06 14:48:20 +0600

    header and navbar updated for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Navbar from '@/components/Navbar';
 
 const Header = () => {
   return (
-    <header className='bg-tertiary sticky top-0 w-full h-20 flex items-cente lg:hidden px-4'>
-      <Navbar/>
+    <header className='bg-tertiary sticky top-0 w-full h-20 flex items-center lg:hidden'>
+      <Navbar />
     </header>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
-import MobileMenu from '@/components/MobileMenu';
 import MobileLogo from '@/components/MobileLogo';
+import MobileMenu from '@/components/MobileMenu';
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between w-full'>
+    <nav className='flex items-center justify-between w-full px-4'>
       <Link href='/'>
         <MobileLogo />
       </Link>
