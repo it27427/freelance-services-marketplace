f150747e8e838f367ee6fe87b82a886a0f552f68
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-06 23:31:30 +0600

    sidebar and layout updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,7 @@ import MainMenu from '@/components/MainMenu';
 
 const Sidebar = () => {
   return (
-    <aside className='bg-gray-200 h-screen lg:w-56 hidden lg:flex flex-col sticky top-0 z-50 p-5 '>
+    <aside className='bg-gray-200 h-screen lg:w-56 flex flex-col sticky top-0 z-50 p-5 '>
       <header className='flex flex-col items-center justify-center'>
         <Link href='/'>
           <BrandLogo />
--- provider/LayoutProvider.tsx
+++ provider/LayoutProvider.tsx
@@ -4,7 +4,10 @@ import Sidebar from '@/components/Sidebar';
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex flex-col lg:flex-row gap-5 h-screen w-full overflow-hidden'>
-      <Sidebar />
+      <div className='hidden lg:flex'>
+        <Sidebar />
+      </div>
+
       <Header />
       
       <main className='p-5 w-full lg:w-[calc(100%-14rem)] ml-auto h-full overflow-y-scroll'>
