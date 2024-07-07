64a93006a29383fa525cc9e43d0de15e6eda263c
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-07 12:31:06 +0600

    isPrivate

--- provider/LayoutProvider.tsx
+++ provider/LayoutProvider.tsx
@@ -1,7 +1,16 @@
+'use client';
+import { usePathname } from 'next/navigation';
+
 import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
+  const pathName = usePathname();
+  const isPrivate =
+    !pathName.includes('sign-in') || !pathName.includes('sign-up');
+
+  
+
   return (
     <div className='flex flex-col lg:flex-row gap-5 h-screen w-full overflow-hidden'>
       <div className='hidden lg:flex'>
@@ -9,7 +18,7 @@ const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
       </div>
 
       <Header />
-      
+
       <main className='p-5 w-full lg:w-[calc(100%-14rem)] ml-auto h-full overflow-y-scroll'>
         {children}
       </main>
