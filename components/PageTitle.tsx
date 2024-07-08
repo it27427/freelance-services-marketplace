fe5d141db6f295baab39b64c7511c0aadc3a6ab9
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-08 17:19:21 +0600

    title font size incresed

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,7 +1,7 @@
 const PageTitle = ({ title }: { title: string }) => {
   return (
     <div>
-      <h1 className='text-xl font-semibold capitalize text-info'>{title}</h1>
+      <h1 className='text-2xl font-semibold capitalize text-info'>{title}</h1>
     </div>
   );
 };
