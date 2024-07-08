d05dc8e55a80a00dd6633d555ada945fa79a2c5a
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-08 10:37:35 +0600

    UserType

---
+++ interfaces/index.ts
@@ -0,0 +1,7 @@
+export interface UserType {
+  _id: number;
+  name: string;
+  email: string;
+  clerkUserId: string;
+  profilePic: string;
+}
--- store/users-store.ts
+++ store/users-store.ts
@@ -1,8 +1,9 @@
+import { UserType } from '@/interfaces';
 import { create } from 'zustand';
 
 const userStore = create((set) => ({
   loggedInUserData: null,
-  SetLoggedInUserData: (data) => set({ loggedInUserData: data }),
+  SetLoggedInUserData: (data: UserType) => set({ loggedInUserData: data }),
 }));
 
 export default userStore;
