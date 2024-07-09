fe9ab6988ee6a92ffe5438715003972ac1bb326d
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-09 11:31:43 +0600

    HomePageHeader file renamed to HomeHeader

--- app/(private)/_common/HomeHeader.tsx
+++ app/(private)/_common/HomeHeader.tsx
@@ -2,7 +2,7 @@
 
 import useUserStore, { UserStoreType } from '@/store/users-store';
 
-const HomePageHeader = () => {
+const HomeHeader = () => {
   const { loggedInUserData } = useUserStore() as UserStoreType;
   return (
     <>
@@ -14,4 +14,4 @@ const HomePageHeader = () => {
   );
 };
 
-export default HomePageHeader;
+export default HomeHeader;
