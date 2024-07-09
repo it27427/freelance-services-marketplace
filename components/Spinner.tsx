722e888416efbec28ba7b9300818476c7991e652
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-09 09:57:46 +0600

    Spinner updated

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -2,7 +2,7 @@ import { Spin } from 'antd';
 
 const Spinner = () => {
   return (
-    <div className='fixed inset-0 z-50 w-full h-screen flex items-center justify-center bg-sky-100'>
+    <div className='fixed inset-0 z-50 w-full h-screen flex items-center justify-center bg-gray-100'>
       <Spin size='large' />
     </div>
   );
