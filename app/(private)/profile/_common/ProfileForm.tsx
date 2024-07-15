be901ed966f37222e5fe30ba590c81cf9db048dc
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-15 09:14:14 +0600

    loggedInUser initialValues

--- app/(private)/profile/_common/ProfileForm.tsx
+++ app/(private)/profile/_common/ProfileForm.tsx
@@ -1,19 +1,19 @@
-"use client";
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 
-import { Button, Form, Input, Tag } from "antd";
+import { Button, Form, Input, Tag } from 'antd';
 import LoadingButton from '@/components/LoadingButton';
-import useUserStore, { UserStoreType } from '@/store/users-store'
+import useUserStore, { UserStoreType } from '@/store/users-store';
 
 const ProfileForm = () => {
   const [skills, setSkills] = useState<string[]>([]);
-  const [skillsValue, setSkillsValue] = useState("");
+  const [skillsValue, setSkillsValue] = useState('');
   const { loggedInUserData }: any = useUserStore() as UserStoreType;
 
   const handleAddSkills = () => {
-    const newSkills = skillsValue.split(",").map((skill) => skill.trim());
+    const newSkills = skillsValue.split(',').map((skill) => skill.trim());
     setSkills([...skills, ...newSkills]);
-    setSkillsValue("");
+    setSkillsValue('');
   };
 
   const handleClose = (removedSkill: string) => {
@@ -24,56 +24,61 @@ const ProfileForm = () => {
 
   const handleSubmit = (value: any) => {};
 
-  const rules = [{required: true, message: 'This field is required.'}]
+  const rules = [{ required: true, message: 'This field is required.' }];
 
   return (
-    <Form layout="vertical" autoComplete="off" onFinish={handleSubmit} initialValues={loggedInUserData}>
-      <div className="md:grid grid-cols-2 gap-5">
-        <Form.Item label="Name" name="name" rules={rules}>
+    <Form
+      layout='vertical'
+      autoComplete='off'
+      onFinish={handleSubmit}
+      initialValues={loggedInUserData}
+    >
+      <div className='md:grid grid-cols-2 gap-5'>
+        <Form.Item label='Name' name='name' rules={rules}>
           <Input />
         </Form.Item>
-        <Form.Item label="Email" name="email" rules={rules}>
+        <Form.Item label='Email' name='email' rules={rules}>
           <Input disabled />
         </Form.Item>
       </div>
 
-      <div className="md:grid grid-cols-2 gap-5">
-        <Form.Item label="Phone" name="phone" rules={rules}>
+      <div className='md:grid grid-cols-2 gap-5'>
+        <Form.Item label='Phone' name='phone' rules={rules}>
           <Input />
         </Form.Item>
-        <Form.Item label="Portfolio" name="portfolio" rules={rules}>
+        <Form.Item label='Portfolio' name='portfolio' rules={rules}>
           <Input />
         </Form.Item>
       </div>
 
-      <Form.Item label="Bio" name="bio" rules={rules}>
+      <Form.Item label='Bio' name='bio' rules={rules}>
         <Input.TextArea rows={6} />
       </Form.Item>
 
-      <div className="flex flex-col gap-2">
-        <h5 className="text-sm m-0 font-normal">
-          Skills <span className="text-xs">(Separated by commas)</span>
+      <div className='flex flex-col gap-2'>
+        <h5 className='text-sm m-0 font-normal'>
+          Skills <span className='text-xs'>(Separated by commas)</span>
         </h5>
-        <div className="flex flex-col gap-5">
-          <div className="flex gap-5">
+        <div className='flex flex-col gap-5'>
+          <div className='flex gap-5'>
             <Input
               value={skillsValue}
               onChange={(e) => setSkillsValue(e.target.value)}
-              placeholder="Enter your skills"
+              placeholder='Enter your skills'
             />
-            <Button onClick={handleAddSkills} type="default">
+            <Button onClick={handleAddSkills} type='default'>
               Add
             </Button>
           </div>
 
-          <div className="flex flex-wrap gap-3 lg:gap-5">
+          <div className='flex flex-wrap gap-3 lg:gap-5'>
             {skills.map((skill, index) => (
               <Tag
                 key={index}
                 closable
                 onClose={() => handleClose(skill)}
-                className="px-5 py-2 bg-secondary text-white m-0"
-                color="secondary"
+                className='px-5 py-2 bg-secondary text-white m-0'
+                color='secondary'
               >
                 {skill}
               </Tag>
@@ -82,8 +87,8 @@ const ProfileForm = () => {
         </div>
       </div>
 
-      <div className="flex justify-end">
-        <LoadingButton onClick={1} index={1} type="primary" htmlType="submit">
+      <div className='flex justify-end'>
+        <LoadingButton onClick={1} index={1} type='primary' htmlType='submit'>
           Submit
         </LoadingButton>
       </div>
