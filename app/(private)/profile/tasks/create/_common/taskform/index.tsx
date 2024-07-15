53b13bf0b50fc74327d2e46ed4dc2a8536e850a5
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-15 12:13:34 +0600

    creating-tab

--- app/(private)/profile/tasks/create/_common/taskform/index.tsx
+++ app/(private)/profile/tasks/create/_common/taskform/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+'use client';
+import { Tabs } from 'antd';
+import Basic from './basic';
+import Description from './description';
+import Attachments from './attachments';
+
+const { TabPane } = Tabs;
 
 const TaskForm = () => {
   return (
     <div>
       <h2>Task Form</h2>
     </div>
-  )
-}
+  );
+};
 
-export default TaskForm
+export default TaskForm;
