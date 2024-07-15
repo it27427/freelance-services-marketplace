11cfae333fc4193d1bc5e476f99b8f109f91b4c6
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-15 11:52:20 +0600

    TaskForm files created

---
+++ app/(private)/profile/tasks/create/_common/taskform/attachments.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Attachments = () => {
+  return (
+    <div>
+      <h2>Attachments</h2>
+    </div>
+  );
+};
+
+export default Attachments;
---
+++ app/(private)/profile/tasks/create/_common/taskform/description.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div>
+      <h2>Description</h2>
+    </div>
+  )
+}
+
+export default Description
