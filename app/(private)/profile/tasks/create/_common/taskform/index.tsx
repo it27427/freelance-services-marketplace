eb7e5948e25f615aa5965722a5f84049f50530eb
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-18 15:39:01 +0600

    isActive

--- app/(private)/profile/tasks/create/_common/taskform/index.tsx
+++ app/(private)/profile/tasks/create/_common/taskform/index.tsx
@@ -27,6 +27,9 @@ const TaskForm = ({
   const [description, setDescription] = useState<string>(
     initialValues?.description || ''
   );
+  const [isActive = true, setIsActive] = useState<boolean>(
+    initialValues?.isActive
+  );
   const [existingAttachments, setExistingAttachments] = useState<any[]>(
     initialValues?.attachments || []
   );
