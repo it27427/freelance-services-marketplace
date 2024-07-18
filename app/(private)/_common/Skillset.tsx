c245fc72262a9818a9122da2d059b7b6e4a6646a
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-18 16:22:13 +0600

    Skillset, Filters and TasksData Created

---
+++ app/(private)/_common/Skillset.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skillset = () => {
+  return (
+    <div>
+      <h1>Skillset</h1>
+    </div>
+  );
+};
+
+export default Skillset;
---
+++ app/(private)/_common/TasksData.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TasksData = () => {
+  return (
+    <div>
+      <h1>TasksData</h1>
+    </div>
+  );
+};
+
+export default TasksData;
