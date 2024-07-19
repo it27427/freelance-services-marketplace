593add805d426b3eb73093272276d301861088a7
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-19 14:04:50 +0600

    task-data visibled

--- app/(private)/_common/TasksData.tsx
+++ app/(private)/_common/TasksData.tsx
@@ -1,3 +1,4 @@
+import { getDateTimeFormat } from '@/helpers/formats';
 import { getAllTasks } from '@/server-actions/tasks';
 import React from 'react';
 
@@ -16,8 +17,25 @@ const TasksData = async () => {
   return (
     <div className='flex flex-col gap-7'>
       {tasks.map((task: any) => (
-        <div>
-          <h2 className='text-lg text-secondary m-0'>{task.title}</h2>
+        <div className='border border-solid border-gray-300 p-5 flex flex-col gap-4'>
+          <hgroup className='flex flex-col gap-2'>
+            <h2 className='text-lg text-secondary m-0'>{task.title}</h2>
+            <h5 className='text-sm text-gray-500 m-0'>{task.subTitle}</h5>
+          </hgroup>
+
+          <div className='flex flex-wrap gap-5'>
+            {task.skillRequired.map((skill: string) => (
+              <span className='bg-secondary text-white px-2 py-1 text-xs'>
+                {skill}
+              </span>
+            ))}
+          </div>
+
+          <div className='flex justify-end'>
+            <span className='text-gray-500 text-xs'>
+              By {task.user.name} on {getDateTimeFormat(task.createdAt)}
+            </span>
+          </div>
         </div>
       ))}
     </div>
