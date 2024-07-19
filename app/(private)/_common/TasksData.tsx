fad209606d29bf16b54b72277ae59f158c4df6cb
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-19 11:50:34 +0600

    No Task Found!

--- app/(private)/_common/TasksData.tsx
+++ app/(private)/_common/TasksData.tsx
@@ -1,6 +1,18 @@
+import { getAllTasks } from '@/server-actions/tasks';
 import React from 'react';
 
 const TasksData = async () => {
+  const response = await getAllTasks();
+  let tasks = [];
+
+  if (response.success) {
+    tasks = response.data;
+  }
+
+  if (tasks.length === 0) {
+    return <span className='text-gray-500'>No Task Found!</span>;
+  }
+
   return (
     <div>
       <h1>TasksData</h1>
