ea0bbb67ebeed4edbc530fd80c9d7060a526927b
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-25 16:53:04 +0600

    Attachments

--- app/(private)/task/[id]/_common/attachments.tsx
+++ app/(private)/task/[id]/_common/attachments.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+'use client';
 import { TaskType } from '@/interfaces';
 
 const Attachments = ({ task }: { task: TaskType }) => {
+
+  const downloadAttachment = async (url: string) => {
+  window.open(url, '_blank');
+  };
+
   return (
-    <div>
-      <h1>Attachments</h1>
+    <div className="flex flex-col gap-4">
+      <h2 className='text-lg text-gray-600 font-bold'>Attachments</h2>
+
+      <div className="flex flex-col gap-5">
+        {
+          task.attachments.map((attachment) => (
+            <div key={attachment.name} className='flex flex-col gap-2 border-gray-200 border-solid p-3'>
+              <span className='text-gray-500 font-semibold'>{attachment.name}</span>
+              <span onClick={() => downloadAttachment(attachment.url)} className='text-blue-500 text-sm cursor-pointer'>Download</span>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 };
