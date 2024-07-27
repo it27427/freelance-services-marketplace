8128336c93159ccacf2418212ff03acfc69d1f37
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-27 12:01:55 +0600

    placebid server-action

--- server-actions/bids.ts
+++ server-actions/bids.ts
@@ -1,7 +1,22 @@
 'use server';
 import connectDB from '@/config/mongodb.connection';
 import Bids from '@/models/bid.model';
+import { message } from 'antd';
+import { create } from 'zustand';
 
 connectDB();
 
-export const placeBid = async (bidData: any) => {};
+export const placeBid = async (bidData: any) => {
+  try {
+    await Bids.create(bidData);
+    return {
+      success: true,
+      message: 'Bid placed successfully!'
+    }
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message
+    }
+  }
+};
