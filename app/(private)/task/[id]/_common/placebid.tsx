d30c40ee988c16fa51a42071e436bed9a10e6347
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-27 11:17:57 +0600

    PlaceBid Button

--- app/(private)/task/[id]/_common/placebid.tsx
+++ app/(private)/task/[id]/_common/placebid.tsx
@@ -1,12 +1,13 @@
+'use client';
 import { Button } from 'antd';
 import { TaskType } from '@/interfaces';
 
 const PlaceBid = ({ task }: { task: TaskType }) => {
   return (
     <div className='flex justify-center'>
-      <Button>Place Bid</Button>
+      <Button type='primary'>Place Bid</Button>
     </div>
   )
 }
 
-export default PlaceBid
+export default PlaceBid;
