e6c7826ce831f90deb8334c5e24717e522a6d613
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-27 11:25:59 +0600

    Place Bid Modal

--- app/(private)/task/[id]/_common/placebid.tsx
+++ app/(private)/task/[id]/_common/placebid.tsx
@@ -1,16 +1,29 @@
 'use client';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import { useState } from 'react';
 import { TaskType } from '@/interfaces';
 
 const PlaceBid = ({ task }: { task: TaskType }) => {
-  const [placeBidModal, setPlaceBidModal] = useState(false);
+  const [showPlaceBidModal, setShowPlaceBidModal] = useState(false);
 
   return (
     <>
       <div className='flex justify-center'>
-        <Button onClick={() => setPlaceBidModal(true)} type='primary'>Place Bid</Button>
+        <Button onClick={() => setShowPlaceBidModal(true)} type='primary'>
+          Place Bid
+        </Button>
       </div>
+
+      {
+        showPlaceBidModal && (
+          <Modal 
+            open={showPlaceBidModal}
+            title='Place Your Bid'
+            centered
+            closable
+            onCancel={() => setShowPlaceBidModal(false)}></Modal>
+        )
+      }
     </>
   )
 }
