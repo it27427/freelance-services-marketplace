da7c0c9be542e04220792e4c3ff3c18cd030d469
it27427 <istiak.tushar.qtec@gmail.com>
2024-07-27 11:54:12 +0600

    Bid-Model Done

--- models/bid.model.ts
+++ models/bid.model.ts
@@ -1,1 +1,39 @@
 import mongoose, { Document } from 'mongoose';
+
+const bidSchema = new mongoose.Schema({
+  bidamount: {
+    type: Number,
+    required: true
+  },
+  deadline: {
+    type: Number,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  freelancer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true
+  },
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true
+  },
+  task: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'tasks',
+    required: true
+  },
+}, { timestamps: true });
+
+if (mongoose.models && mongoose.models['Bids']) {
+  delete mongoose.models['Bids'];
+}
+
+const Bid: any = mongoose.model('Bids', bidSchema);
+
+export default Bid;
